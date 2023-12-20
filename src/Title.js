e72f53bafeb02b39c8db844967166e5e904fa702
Rhéda Hachemi <rheda@hachemi.fr>
2023-12-20 22:58:51 +0100

    header component built

---
+++ src/Title.js
@@ -0,0 +1,19 @@
+// src/Title.js
+
+ 
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    
+    <div className="Title">
+      <h1>PAGODE</h1>
+      
+     <div className="Title-subtitle">Histoire et patrimoine du GODF</div>
+     </div>
+  )
+}
+
+export default Title
